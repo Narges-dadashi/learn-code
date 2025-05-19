cd87079e917627d8a70dde714b7b4e12f8ed88df
Narges-dadashi <nargesdadashi7@gmail.com>
2025-05-19 16:25:23 +0330

    BaseApiController

--- api/GlobalUsing.cs
+++ api/GlobalUsing.cs
@@ -7,3 +7,4 @@ global using Microsoft.Extensions.Options;
 global using MongoDB.Driver;
 global using MongoDB.Bson;
 global using MongoDB.Bson.Serialization.Attributes;
+global using api.Controllers.Helpers;
