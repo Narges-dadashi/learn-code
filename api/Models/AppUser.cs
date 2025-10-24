667e688b3c00816f1c86c09f007d9d03cccb74af
Narges-dadashi <nargesdadashi7@gmail.com>
2025-10-24 17:36:13 +0330

    Convert record to class

--- api/Models/AppUser.cs
+++ api/Models/AppUser.cs
@@ -1,12 +1,27 @@
 namespace api.Models;
 
-public record AppUser(
-[property: BsonId, BsonRepresentation(BsonType.ObjectId)] string? Id,
-[EmailAddress] string Email,
-string Name,
-string Password,
-string ConfirmPassword,
-DateOnly DateOfBirth,
-string Gender,
-string Country
-);
+public class AppUser
+{
+    [property: BsonId, BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; init; }
+    [EmailAddress]
+    public string Email { get; init; } = string.Empty;
+    [StringLength(30, MinimumLength = 3)]
+    public string Name { get; init; } = string.Empty;
+    public string Password { get; init; } = string.Empty;
+    public string ConfirmPassword { get; init; } = string.Empty;
+    public DateTime LastActive { get; init; }
+    public string Gender { get; init; } = string.Empty;
+    public string Country { get; init; } = string.Empty;
+}
+
+// public record AppUser(
+// [property: BsonId, BsonRepresentation(BsonType.ObjectId)] string? Id,
+// [EmailAddress] string Email,
+// string Name,
+// string Password,
+// string ConfirmPassword,
+// DateOnly DateOfBirth,
+// string Gender,
+// string Country
+// );
